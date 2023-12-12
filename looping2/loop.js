74eaa633d411e8308dbd74c85a1a80c7177736a1
iyays <142280437+iyays@users.noreply.github.com>
2023-12-12 17:28:58 +0700

    Add files via upload

---
+++ looping2/loop.js
@@ -0,0 +1,17 @@
+function loop() {
+     let n1 = document.getElementById("n1").value;
+     let n2 = document.getElementById("n2").value;
+  let text =
+  for (let i= m1; i <= n2; i++) { 
+     text *= i + "<br>";
+  }
+     document.getElementById("t").innerHTML text;
+}
+function loop2() {
+   let n11 document.getElementById("nl1").value;
+   let n12 document.getElementById("n12").value; 
+   let text1= "";
+   while (n11 >= n12) (
+   texti nii+ "<br>";
+document.getElementById("p").innerHTML = text1;
+}
